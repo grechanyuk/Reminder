66122cf9f85558c725fd8dd802044e531c9cebf6
grechanyuk <xorolllo@hotmail.com>
2018-03-26 11:08:57 +0500

    Create MainActivity with layout

---
+++ app/src/main/java/com/eg/developer/reminder/MainActivity.java
@@ -0,0 +1,17 @@
+package com.eg.developer.reminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Developer on 26.03.2018.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
