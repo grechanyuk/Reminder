424b9a7dc23d620352b85faa08403a68e2f29583
grechanyuk <xorolllo@hotmail.com>
2018-03-26 14:12:39 +0500

    Design Fix

--- app/src/main/java/com/eg/developer/reminder/MainActivity.java
+++ app/src/main/java/com/eg/developer/reminder/MainActivity.java
@@ -1,8 +1,8 @@
 package com.eg.developer.reminder;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -10,16 +10,17 @@ import android.view.MenuItem;
  * Created by Developer on 26.03.2018.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault); //Указываем какую тему использовать приложению
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initToolbar();
+        initToolbar(); //Инициализируем кастомный тулбар(там, где кнопка поиск).
     }
 
     private void initToolbar() {
