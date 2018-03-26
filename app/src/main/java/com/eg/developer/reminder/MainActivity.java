69f4168dcdf28da98d921b75e917d3ec8332256c
grechanyuk <xorolllo@hotmail.com>
2018-03-26 13:18:51 +0500

    Toolbar added

--- app/src/main/java/com/eg/developer/reminder/MainActivity.java
+++ app/src/main/java/com/eg/developer/reminder/MainActivity.java
@@ -3,15 +3,36 @@ package com.eg.developer.reminder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Developer on 26.03.2018.
  */
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
